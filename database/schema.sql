951004a9ffd2f449c74c6296fbf4ecbfdb126fa9
sudipto818 <sudiptoghosh096@gmail.com>
2026-03-30 23:07:08 +0530

    Created the database schema
    Created two tables for better maintenance of the data.

---
+++ database/schema.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS aqi_db;
+USE aqi_db;
+
+CREATE TABLE cities (
+    city_id INT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(50) UNIQUE,
+    state VARCHAR(50),
+    region VARCHAR(50)
+);
+
+CREATE TABLE aqi_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,   -- temporary PK
+    city_id INT,
+    city VARCHAR(50),                    -- temporary (for CSV load)
+    date DATE,
+    co FLOAT,
+    no2 FLOAT,
+    o3 FLOAT,
+    pm10 FLOAT,
+    pm25 FLOAT,
+    aqi_daily FLOAT  
+);
